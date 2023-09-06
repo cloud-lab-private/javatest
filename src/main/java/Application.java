d4a548c280e00defb014501bec4814a9448cf80a
samargitworld <124122212+samargitworld@users.noreply.github.com>
2023-09-06 12:19:36 +0530

    Update Application.java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -3,7 +3,7 @@ public class Application {
     /**
      * This class contains a main method that allows you to manually test the Hello World lab functionality.
      * No changes you make here affect the test case results. You must still run & pass the test cases.
-     * All implementation code should be written in the Lab class.
+     * All implementation code should be written in the Lab class. from parent repo.
      *
      * The Lab.sayHello method should produce a value, "Hello, World!", which this class should be able to print
      * when you run the main method.
